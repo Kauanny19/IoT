4cb4937b46b66a10c9c40fe20b8dc6b91d9bba8a
Kauanny19 <kauannycristina01n@gmail.com>
2025-09-15 09:02:18 -0300

    08/09

--- Monitor_temperatura/Monitor_temperatura.ino
+++ Monitor_temperatura/Monitor_temperatura.ino
@@ -1,41 +1,58 @@
 #include <math.h>
 #include "AdafruitIO_WiFi.h"
+#include "NewPing.h" // instalar bibiotacas *
 
-//configurações da rede Wifi
+//Configuração da rede WIFI
 #define WIFI_SSID "..."
 #define WIFI_PASS "..."
 
-//Autenticação Adafruit IO
+// Autenticação Adafruit IO
 #define IO_USERNAME "..."
 #define IO_KEY "..."
 
 AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASS);
 
-#define pinNTC 34
-#define pinLed 14 // Pino do LED
+#define pinNTC 34 // pino do sensor de temperatura
+//#define pinLed 14 // pino do led
 
-//Controle de envio de dados
+#define BUZZER_PIN 27
+#define LED_ALARME 13
+#define BOTAO_FISICO 26
+#define TRIG_PIN 12
+#define ECHO_PIN 14
+
+//Configuração Ultrassonico
+#define MAX_DISTANCE 100
+NewPing sonar(TRIG_PIN, ECHO_PIN, MAX_DISTANCE);
+
+// Controle de envio de dados 
 float temp_atual = 0;
 float temp_anterior = -1;
 
-//Variável / ponteiro para referenciar o feed temperatura
-AdafruitIO_Feed *botaoled = io.feed("botaoled");
+//Variavel / ponteiro para referenciar o feed temperatura
+AdafruitIO_Feed *temperatura = io.feed("Temperatura");
+AdafruitIO_Feed *ledFeed = io.feed("botaoled");
+AdafruitIO_Feed *botaoalarme = io.feed("botaoalarme");
 
-const float Rfixo = 10000.0; //Resistor do projeto
-const float Beta = 3950.0;
-const float R0 = 10000; // Nominal do ntc
+
+
+const float Rfixo = 10000.0; // Resistor do projeto 
+const float Beta = 3950.0; 
+const float R0 = 10000.0; // nominal do NTC
 const float T0_kelvin = 298.15; // 25°C em Kelvin
 const float Vcc = 3.3;
 
 void setup() {
   pinMode(pinNTC, INPUT);
-  pinMode(pinLed, OUTPUT);
-
+  //pinMode(pinLed, OUTPUT);
+  pinMode(BUZZER_PIN, OUTPUT);
+  pinMode(LED_ALARME, OUTPUT);
+  pinMode(BOTAO_FISICO, INPUT);
   Serial.begin(115200);
 
   while(!Serial);
 
-  Serial.print("Conectando ao Adafruit IO");
+  Serial.print("Conectando ao adafruit IO");
   io.connect();
 
   while(io.status() < AIO_CONNECTED){
@@ -47,19 +64,25 @@ void setup() {
   Serial.println(io.statusText());
 
   // Configuração do callback, quando o feed receber(atualizar) um valor
-  botaoled -> onMessage(handleBotaoLed);
-  // Registra a função de callback 
-  // Ela será chamada sempre que o feed receber um novo dado
+  // temperatura -> onMessage(handleTemperatura);
+  ledFeed->onMessage(handleBotaoLed);
+  //registra a função de callback 
+  // ela será chamada sempre que o feed receber um novo dado
 
-  delay(1000);
+
+
+  delay(2000);
 }
 
 void loop() {
+  //Manter a coneção com o Adafruit IO ativa
+  //io.run();
+  //publicacao(); //chamada da função publish
 
-  //Manter a conexão com o Adafruit IO ativa
-  io.run();
-  
-  //publicacao(); // Chamada da função publish
-
-  delay(3000);
+  Serial.print(F("Distancia lida: "));
+  Serial.println(sonar.ping_cm());
+  testeLed();
+  testeBuzzer();
+  testeBotao(BOTAO_FISICO);
+  delay(500);
 }
